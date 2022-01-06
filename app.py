1e125c51127a99e0e88956b9389471a30bee494e
tosha-abadilla <89688262+tosha-abadilla@users.noreply.github.com>
2022-01-06 17:52:04 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask,redirect
+from flask import render_template
+from flask import request
+from flask import session
+from bson.json_util import loads, dumps
+from flask import make_response
+import database as db
+import authentication
+import logging
+import ordermanagement as om
+
+
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes.
+# Keep this really secret!
+app.secret_key = b's@g@d@c0ff33!'
+
+
+logging.basicConfig(level=logging.DEBUG)
+app.logger.setLevel(logging.INFO)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', page="Index")
+
+@app.route('/products')
+def products():
+    product_list = db.get_products()
+    return render_template('products.html', page="Products", product_list=product_list)
+
+@app.route('/productdetails')
+def productdetails():
+    code = request.args.get('code', '')
+    product = db.get_product(int(code))
+
+    return render_template('productdetails.html', code=code, product=product)
+
+@app.route('/branches')
+def branches():
+    return render_template('branches.html', page="Branches")
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html', page="About Us")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/auth', methods = ['GET', 'POST'])
+def auth():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    is_successful, user = authentication.login(username, password)
+    app.logger.info('%s', is_successful)
+    if(is_successful):
+        session["user"] = user
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session.pop("user",None)
+    session.pop("cart",None)
+    return redirect('/')
+
+@app.route('/addtocart')
+def addtocart():
+    code = request.args.get('code', '')
+    product = db.get_product(int(code))
+    item=dict()
+    # A click to add a product translates to a
+    # quantity of 1 for now
+
+    item["qty"] = 1
+    item["name"] = product["name"]
+    item["subtotal"] = product["price"]*item["qty"]
+
+    if(session.get("cart") is None):
+        session["cart"]={}
+
+    cart = session["cart"]
+    cart[code]=item
+    session["cart"]=cart
+    return redirect('/cart')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/checkout')
+def checkout():
+    # clear cart in session memory upon checkout
+    om.create_order_from_cart()
+    session.pop("cart",None)
+    return redirect('/ordercomplete')
+
+@app.route('/ordercomplete')
+def ordercomplete():
+    return render_template('ordercomplete.html')
+
+@app.route('/api/products',methods=['GET'])
+def api_get_products():
+    resp = make_response( dumps(db.get_products()) )
+    resp.mimetype = 'application/json'
+    return resp
---
+++ database.py
@@ -0,0 +1,51 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+products_db = myclient["products"]
+order_management_db = myclient["order_management"]
+
+products = {
+    100: {"name":"Americano","price":125},
+    200: {"name":"Brewed Coffee","price":110},
+    300: {"name":"Cappuccino","price":120},
+    400: {"name":"Espresso","price":120},
+    500: {"name":"Latte","price":140},
+    600: {"name":"Cold Brew","price":200}
+}
+
+branches = {
+    1: {"name":"Katipunan"},
+    2: {"name":"Tomas Morato"},
+    3: {"name":"Eastwood"},
+    4: {"name":"Tiendesitas"},
+    5: {"name":"Arcovia"},
+}
+
+
+def get_product(code):
+    products_coll = products_db["products"]
+
+    product = products_coll.find_one({"code":code},{"_id":0})
+
+    return product
+
+def get_products():
+    product_list = []
+
+    products_coll = products_db["products"]
+
+    for p in products_coll.find({},{"_id":0}):
+        product_list.append(p)
+
+    return product_list
+
+
+def get_user(username):
+    customers_coll = order_management_db['customers']
+    user=customers_coll.find_one({"username":username})
+    return user
+
+def create_order(order):
+    orders_coll = order_management_db['orders']
+    orders_coll.insert(order)
